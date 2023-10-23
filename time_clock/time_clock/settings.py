a9e9f0970a9c965782937fd7e2eb7d54cd2deec2
Benjamin Lowry <Lowry_46@hotmail.com>
2023-10-23 08:54:27 -0400

    updating

--- time_clock/time_clock/settings.py
+++ time_clock/time_clock/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "timesheet.apps.TimesheetConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "EST"
 
 USE_I18N = True
 
